b14a9f1f9135b721759e096c4c83298c3ed0a7c6
MAYTheUser <muh.4lief@gmail.com>
2026-06-30 21:34:05 +0700

    store 2

--- store/store_ui.gd
+++ store/store_ui.gd
@@ -5,28 +5,33 @@ extends Control
 var storeItemDb = StoreItems.testStore
 var playerInv = InvManager.inv
 var leftBarState:String = "options"
-var leftBarChildren = $leftBar.get_children()
+var leftBarChildren:Array
 
-@onready var itemBar = $leftBar/itemBar/itemList
-@onready var option = $leftBar/sellItem/optionList
-@onready var descPanelName = $descPanel/VBoxContainer/Control/name
-@onready var descPanelDesc = $descPanel/VBoxContainer/Control/desc
-@onready var descPanelPrice = $descPanel/VBoxContainer/Control/price
+@onready var itemList = $leftBar/itemBar/itemList
+@onready var itemBar = $leftBar/itemBar
+@onready var options = $leftBar/options
+@onready var optionList = $leftBar/options/optionlList
+@onready var talkBar = $leftBar/talkBar
+@onready var talkList = $leftBar/talkBar/talkList
+@onready var itemInfo = $descPanel/VBoxContainer/itemInfo
+@onready var descPanelName = $descPanel/VBoxContainer/itemInfo/name
+@onready var descPanelDesc = $descPanel/VBoxContainer/itemInfo/desc
+@onready var descPanelPrice = $descPanel/VBoxContainer/itemInfo/price
 @onready var dialoguePanel = $descPanel/VBoxContainer/dialogue
 
 func _ready() -> void:
-	leftBarState = "options"
-	for node in leftBarChildren:
-		if node.name != "options":
-			node.visible = false
+	left_bar_state_ctrl("options")
 	
-	$descPanel/VBoxContainer/Control.visible = false
+	itemInfo.visible = false
 	
 	dialoguePanel.text = "Hello Stranger, what a nice day to take a walk. May you interested to buy or sell something"
 	
 	populate_list()
 
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("back"):
+		left_bar_state_ctrl("options")
+	
 	focus_ctrl()
 
 func focus_ctrl():
@@ -34,20 +39,27 @@ func focus_ctrl():
 	
 	
 	if focusedNode:
-		if leftBarState == "buy":
-			var key = focusedNode.get_parent().name
-			for item in storeItemDb:
-				if item.name == key:
-					descPanelName.text = item.name
-					descPanelDesc.text = item.desc
-					descPanelPrice.text = str(item.buyPrice) + "$"
-					break
+		match leftBarState:
+			"buy":
+				var key = focusedNode.get_parent().name
+				for item in storeItemDb:
+					if item.name == key:
+						descPanelName.text = item.name
+						descPanelDesc.text = item.desc
+						descPanelPrice.text = str(item.buyPrice) + "$"
+						break
 	else:
 		if  Input.is_action_just_pressed("ui_down"):
-			itemBar.get_child(0).find_child("itemButton").grab_focus()
+			match leftBarState:
+				"buy":
+					itemList.get_child(0).find_child("itemButton").grab_focus()
+				"sell":
+					pass
+				"options":
+					optionList.get_child(0).grab_focus()
 
 func populate_list():
-	for child in itemBar.get_children():
+	for child in itemList.get_children():
 		child.free()
 	
 	for item in storeItemDb:
@@ -56,13 +68,53 @@ func populate_list():
 		row.name = item.name
 		row.find_child("name").text = item.name
 		row.find_child("price").text = str(item.buyPrice)
-		itemBar.add_child(row)
+		itemList.add_child(row)
+	
+	leftBarChildren = $leftBar.get_children()
 
-func left_bar_state_ctrl(leftBarState:String):
+func left_bar_state_ctrl(new_state:String):
+	leftBarState = new_state
+	
 	match leftBarState:
 		"buy":
-			for node in leftBarChildren:
-				pass
+			leftBarState = "buy"
+			
+			itemBar.visible = true
+			options.visible = false
+			talkBar.visible = false
+			itemInfo.visible = true
+			dialoguePanel.visible = false
+		"sell":
+			leftBarState = "sell"
+			
+			itemBar.visible = true
+			options.visible = false
+			talkBar.visible = false
+			itemInfo.visible = true
+			dialoguePanel.visible = false
+		"options":
+			leftBarState = "options"
+			
+			itemBar.visible = false
+			options.visible = true
+			talkBar.visible = false
+		"talk":
+			leftBarState = "talk"
+			
+			itemBar.visible = false
+			options.visible = false
+			talkBar.visible = true
+		_:
+			pass
 
 func _on_buy_pressed() -> void:
+	get_viewport().gui_release_focus()
 	left_bar_state_ctrl("buy")
+
+func _on_sell_pressed() -> void:
+	get_viewport().gui_release_focus()
+	left_bar_state_ctrl("sell")
+
+func _on_talk_pressed() -> void:
+	get_viewport().gui_release_focus()
+	left_bar_state_ctrl("talk")
